a89da2f339083ca21c35145f6c1e8a78a1567c7d
William Timoney <wtimoney@140-sfoengwifi55-160.clients.corp.yelpcorp.com>
2015-05-14 13:56:44 -0700

    --

--- presto-elasticsearch/src/test/java/com/wrmsr/presto/elasticsearch/TestEverything.java
+++ presto-elasticsearch/src/test/java/com/wrmsr/presto/elasticsearch/TestEverything.java
@@ -5,6 +5,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 public class TestEverything
 {
     protected final ElasticsearchTestHelper helper = new ElasticsearchTestHelper();
@@ -27,5 +29,31 @@ public class TestEverything
     {
         helper.startNode("server1");
         helper.waitForGreen("server1", "10s");
+    }
+
+    @Test
+    public void testClusterSettingsTransmit() throws Exception {
+        helper.startNode("server1");
+        helper.waitForGreen("server1", "10s");
+        helper.startNode("server2");
+        helper.waitForGreen("server2", "10s");
+
+        Map response = helper.httpClient("server1").request("_cluster/settings").response();
+        assert ((Map) response.get("persistent")).size() == 0;
+        response = helper.httpClient("server1").request("PUT", "_cluster/settings", "{\"persistent\": {\"cluster.blocks.read_only\": true}}".getBytes()).response();
+        assert (Boolean) response.get("acknowledged") == true;
+
+        response = helper.httpClient("server1").request("_cluster/settings").response();
+        assert ((String) helper.getDeep(response, "persistent", "cluster", "blocks", "read_only")).equals("true");
+        response = helper.httpClient("server2").request("_cluster/settings").response();
+        assert ((String) helper.getDeep(response, "persistent", "cluster", "blocks", "read_only")).equals("true");
+
+        helper.closeNode("server1");
+
+        helper.startNode("server3");
+        helper.waitForGreen("server3", "10s");
+
+        response = helper.httpClient("server3").request("_cluster/settings").response();
+        assert ((String) helper.getDeep(response, "persistent", "cluster", "blocks", "read_only")).equals("true");
     }
 }
