d2c7165f0b2ea0b4b7df1d841fd6d2ddbc9a09d9
William Timoney <wtimoney@140-sfoengwifi55-160.clients.corp.yelpcorp.com>
2015-05-15 18:30:12 -0700

    --

---
+++ presto-wrmsr-wrapper/src/test/java/com/wrmsr/presto/materializer/TestMaterializerPlugin.java
@@ -0,0 +1,67 @@
+package com.wrmsr.presto.materializer;
+
+import com.facebook.presto.Session;
+import com.facebook.presto.metadata.FunctionFactory;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.testing.LocalQueryRunner;
+import com.facebook.presto.tests.AbstractTestQueryFramework;
+import com.facebook.presto.tpch.TpchConnectorFactory;
+import com.facebook.presto.type.ParametricType;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.spi.type.TimeZoneKey.UTC_KEY;
+import static com.facebook.presto.tpch.TpchMetadata.TINY_SCHEMA_NAME;
+import static java.util.Locale.ENGLISH;
+
+public class TestMaterializerPlugin
+        extends AbstractTestQueryFramework
+{
+    public TestMaterializerPlugin()
+    {
+        super(createLocalQueryRunner());
+    }
+
+    @Test
+    public void testSanity()
+            throws Exception
+    {
+        queryRunner.execute("select * from lineitem inner join orders on orders.orderkey = lineitem.orderkey inner join customer on orders.custkey = customer.custkey limit 10");
+    }
+
+    private static LocalQueryRunner createLocalQueryRunner()
+    {
+        Session defaultSession = Session.builder()
+                .setUser("user")
+                .setSource("test")
+                .setCatalog("local")
+                .setSchema(TINY_SCHEMA_NAME)
+                .setTimeZoneKey(UTC_KEY)
+                .setLocale(ENGLISH)
+                .build();
+
+        LocalQueryRunner queryRunner = new LocalQueryRunner(defaultSession);
+
+        // add the tpch catalog
+        // local queries run directly against the generator
+        queryRunner.createCatalog(
+                defaultSession.getCatalog(),
+                new TpchConnectorFactory(queryRunner.getNodeManager(), 1),
+                ImmutableMap.<String, String>of());
+
+        MaterializerPlugin plugin = new MaterializerPlugin();
+        plugin.setTypeManager(queryRunner.getTypeManager());
+        /*
+        for (Type type : plugin.getServices(Type.class)) {
+            queryRunner.getTypeManager().addType(type);
+        }
+        for (ParametricType parametricType : plugin.getServices(ParametricType.class)) {
+            queryRunner.getTypeManager().addParametricType(parametricType);
+        }
+        */
+        // queryRunner.getMetadata().getFunctionRegistry().addFunctions(Iterables.getOnlyElement(plugin.getServices(FunctionFactory.class)).listFunctions());
+
+        return queryRunner;
+    }
+}
---
+++ presto-wrmsr-yarn/src/main/java/com/wrmsr/presto/materializer/MaterializerPlugin.java
@@ -0,0 +1,47 @@
+package com.wrmsr.presto.materializer;
+
+// import com.facebook.presto.metadata.FunctionFactory;
+import com.facebook.presto.spi.Plugin;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
+// import com.facebook.presto.type.ParametricType;
+import com.google.common.collect.ImmutableList;
+
+import javax.inject.Inject;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class MaterializerPlugin
+        implements Plugin
+{
+    private TypeManager typeManager;
+
+    @Override
+    public void setOptionalConfig(Map<String, String> optionalConfig)
+    {
+    }
+
+    @Inject
+    public void setTypeManager(TypeManager typeManager)
+    {
+        this.typeManager = checkNotNull(typeManager, "typeManager is null");
+    }
+
+    @Override
+    public <T> List<T> getServices(Class<T> type)
+    {
+//        if (type == FunctionFactory.class) {
+//            return ImmutableList.of(type.cast(new MLFunctionFactory(typeManager)));
+//        }
+//        else if (type == Type.class) {
+//            return ImmutableList.of(type.cast(MODEL), type.cast(REGRESSOR));
+//        }
+//        else if (type == ParametricType.class) {
+//            return ImmutableList.of(type.cast(new ClassifierParametricType()));
+//        }
+        return ImmutableList.of();
+    }
+}
