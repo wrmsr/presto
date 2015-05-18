7b0e14f93afedbb30e06b63ddaa64d36ce8ce0b4
William Timoney <wtimoney@140-sfoengwifi53-229.clients.corp.yelpcorp.com>
2015-05-18 14:04:50 -0700

    --

--- presto-wrmsr-yarn/src/test/java/com/wrmsr/presto/yarn/TestYarnPlugin.java
+++ presto-wrmsr-yarn/src/test/java/com/wrmsr/presto/yarn/TestYarnPlugin.java
@@ -1,24 +1,33 @@
-package com.wrmsr.presto.materializer;
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wrmsr.presto.yarn;
 
 import com.facebook.presto.Session;
-import com.facebook.presto.metadata.FunctionFactory;
-import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.testing.LocalQueryRunner;
 import com.facebook.presto.tests.AbstractTestQueryFramework;
 import com.facebook.presto.tpch.TpchConnectorFactory;
-import com.facebook.presto.type.ParametricType;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Iterables;
 import org.testng.annotations.Test;
 
 import static com.facebook.presto.spi.type.TimeZoneKey.UTC_KEY;
 import static com.facebook.presto.tpch.TpchMetadata.TINY_SCHEMA_NAME;
 import static java.util.Locale.ENGLISH;
 
-public class TestMaterializerPlugin
+public class TestYarnPlugin
         extends AbstractTestQueryFramework
 {
-    public TestMaterializerPlugin()
+    public TestYarnPlugin()
     {
         super(createLocalQueryRunner());
     }
@@ -50,7 +59,7 @@ public class TestMaterializerPlugin
                 new TpchConnectorFactory(queryRunner.getNodeManager(), 1),
                 ImmutableMap.<String, String>of());
 
-        MaterializerPlugin plugin = new MaterializerPlugin();
+        YarnPlugin plugin = new YarnPlugin();
         plugin.setTypeManager(queryRunner.getTypeManager());
         /*
         for (Type type : plugin.getServices(Type.class)) {
