9a0dc05ab2151aab5e31bee8fe13a460f4dad992
William Timoney <wtimoney@140-sfoengwifi53-229.clients.corp.yelpcorp.com>
2015-05-22 17:32:50 -0700

    --

--- presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperMain.java
+++ presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperMain.java
@@ -13,14 +13,80 @@
  */
 package com.wrmsr.presto.wrapper;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.wrmsr.presto.wrapper.util.ParentLastURLClassLoader;
+import io.airlift.resolver.ArtifactResolver;
+import io.airlift.log.Logger;
+import org.sonatype.aether.artifact.Artifact;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
 public class PrestoWrapperMain
 {
     private PrestoWrapperMain()
     {
     }
 
-    public static void main(String[] args) throws Throwable
+    public static void main(String[] args)
+            throws Throwable
     {
-        System.out.println("hi");
+        new PrestoWrapperMain().run(args);
     }
+
+    public void run(String[] args)
+            throws Throwable
+    {
+        Logger log = Logger.get(PrestoWrapperMain.class);
+
+        ImmutableMap<String, String> props = ImmutableMap.<String, String>builder()
+                .put("node.environment", "production")
+                .put("node.id", "ffffffff-ffff-ffff-ffff-ffffffffffff")
+                .put("node.data-dir", "/Users/wtimoney/presto/data/")
+                .put("presto.version", "0.103")
+                .put("coordinator", "true")
+                .put("node-scheduler.include-coordinator", "true")
+                .put("http-server.http.port", "8080")
+                .put("task.max-memory", "1GB")
+                .put("discovery-server.enabled", "true")
+                .put("discovery.uri", "http://localhost:8080")
+                .build();
+        for (Map.Entry<String, String> e : props.entrySet()) {
+            System.setProperty(e.getKey(), e.getValue());
+        }
+
+        File cwd = new File(System.getProperty("user.dir"));
+
+        ArtifactResolver resolver = new ArtifactResolver(
+                ArtifactResolver.USER_LOCAL_REPO,
+                ImmutableList.of(ArtifactResolver.MAVEN_CENTRAL_URI));
+        List<Artifact> artifacts = resolver.resolvePom(new File(cwd, "presto-main/pom.xml"));
+
+        List<File> files = Lists.newArrayList(new File(cwd, "presto-main/target/classes/"));
+        for (Artifact a : artifacts) {
+            files.add(a.getFile());
+        }
+
+        List<URL> urls = Lists.newArrayList();
+        for (File file : files) {
+            if (!file.exists()) {
+                throw new IllegalStateException(file.getAbsolutePath());
+            }
+            URL url = new URL("file:" + file.getAbsolutePath() + (file.isDirectory() ? "/" : ""));
+            System.out.println(url);
+            urls.add(url);
+        }
+
+        ClassLoader cl = new ParentLastURLClassLoader(urls);
+
+        Class prestoServerClass = cl.loadClass("com.facebook.presto.server.PrestoServer");
+        Method prestoServerMain = prestoServerClass.getMethod("main", String[].class);
+        prestoServerMain.invoke(null, new Object[]{ args });
+    }
+
 }
