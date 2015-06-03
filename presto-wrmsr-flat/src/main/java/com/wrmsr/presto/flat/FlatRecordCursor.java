491fec78b8a5a686d1a9a22be8f52602a7a83f8a
William Timoney <wtimoney@140-sfoengwifi53-229.clients.corp.yelpcorp.com>
2015-06-03 14:14:53 -0700

    --

--- presto-wrmsr-flat/src/main/java/com/wrmsr/presto/flat/FlatRecordCursor.java
+++ presto-wrmsr-flat/src/main/java/com/wrmsr/presto/flat/FlatRecordCursor.java
@@ -3,18 +3,43 @@ package com.wrmsr.presto.flat;
 import com.facebook.presto.spi.RecordCursor;
 import com.facebook.presto.spi.type.Type;
 import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import javax.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class FlatRecordCursor
     implements RecordCursor
 {
+    private final FileInputStream f;
+    private final BufferedReader br;
+    @Nullable
+    private String next = null;
 
     public FlatRecordCursor()
     {
         try {
+            f = new FileInputStream(new File("/Users/wtimoney/presto/flat/hi.txt"));
+            br = new BufferedReader(new InputStreamReader(f));
 
-
+            /*
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.printf("%s%n", line);
+            }
+            */
         }
         catch (IOException e) {
             throw new RuntimeException(e);
@@ -42,37 +67,58 @@ public class FlatRecordCursor
     @Override
     public Type getType(int field)
     {
-        return null;
+        checkArgument(field == 0);
+        return FlatMetadata.COLUMN_TYPE;
     }
 
     @Override
     public boolean advanceNextPosition()
     {
-        return false;
+        if (next != null) {
+            return true;
+        }
+        try {
+            next = br.readLine();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return next != null;
     }
 
     @Override
     public boolean getBoolean(int field)
     {
-        return false;
+        throw new NotImplementedException();
     }
 
     @Override
     public long getLong(int field)
     {
-        return 0;
+        throw new NotImplementedException();
     }
 
     @Override
     public double getDouble(int field)
     {
-        return 0;
+        throw new NotImplementedException();
     }
 
     @Override
     public Slice getSlice(int field)
     {
-        return null;
+        if (next == null) {
+            try {
+                next = br.readLine();
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            checkNotNull(next);
+        }
+        String ret = next;
+        next = null;
+        return Slices.wrappedBuffer(ret.getBytes());
     }
 
     @Override
@@ -84,6 +130,12 @@ public class FlatRecordCursor
     @Override
     public void close()
     {
-
+        try {
+            br.close();
+            f.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
