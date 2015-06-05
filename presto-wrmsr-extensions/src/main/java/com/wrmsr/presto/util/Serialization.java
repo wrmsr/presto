cb30b8e0da803cde2bcdde2ad4b16b76a76581bb
FUCKYOUGIT <FUCKYOUGIT@FUCKYOU.FUCK>
2015-06-05 03:39:37 -0700

    --

---
+++ presto-wrmsr-extensions/src/main/java/com/wrmsr/presto/util/Serialization.java
@@ -0,0 +1,80 @@
+package com.wrmsr.presto.util;
+
+import com.facebook.presto.server.SliceSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.DefaultDeserializationContext;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import io.airlift.json.ObjectMapperProvider;
+import io.airlift.slice.Slice;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class Serialization
+{
+    public static final Supplier<ObjectMapper> OBJECT_MAPPER = Suppliers.memoize(() ->
+            new ObjectMapperProvider().get()
+                    .registerModule(new SimpleModule().addSerializer(Slice.class, new SliceSerializer())));
+
+    public static final Supplier<ObjectMapper> YAML_OBJECT_MAPPER = Suppliers.memoize(() -> {
+        ObjectMapper mapper = OBJECT_MAPPER.get();
+        return new ObjectMapper(
+                new YAMLFactory(),
+                (DefaultSerializerProvider) mapper.getSerializerProvider(),
+                (DefaultDeserializationContext) mapper.getDeserializationContext());
+    });
+
+    public static final ThreadLocal<Yaml> YAML = ThreadLocal.withInitial(Yaml::new);
+
+    @SuppressWarnings({"unchecked"})
+    public static Map<String, String> flattenYaml(String key, Object value)
+    {
+        /*
+        TODO:
+        !!binary	byte[], String
+        !!timestamp	java.util.Date, java.sql.Date, java.sql.Timestamp
+        !!omap, !!pairs	List of Object[]
+        */
+        if (value == null) {
+            Map<String, String> map = Maps.newHashMap();
+            map.put(key, null);
+            return map;
+        }
+        else if (value instanceof Map) {
+            Map<String, Object> mapValue = (Map<String, Object>) value;
+            return mapValue.entrySet().stream()
+                    .flatMap(e -> flattenYaml((key != null ? key + "." : "") + e.getKey(), e.getValue()).entrySet().stream())
+                    .collect(ImmutableCollectors.toImmutableMap(e -> e.getKey(), e -> e.getValue()));
+        }
+        else if (value instanceof List || value instanceof Set) {
+            List<Object> listValue = ImmutableList.copyOf((Iterable<Object>) value);
+            return IntStream.range(0, listValue.size()).boxed()
+                    .flatMap(i -> flattenYaml(key + "(" + i.toString() + ")", listValue.get(i)).entrySet().stream())
+                    .collect(ImmutableCollectors.toImmutableMap(e -> e.getKey(), e -> e.getValue()));
+        }
+        else {
+            return ImmutableMap.of(key, value.toString());
+        }
+    }
+
+    public static List<Object> splitYaml(String src)
+    {
+        return ImmutableList.copyOf(YAML.get().loadAll(new ByteArrayInputStream(src.getBytes())));
+    }
+
+    public static String renderYaml(Object obj)
+    {
+        return YAML.get().dump(obj);
+    }
+}
--- presto-wrmsr-extensions/src/test/java/com/wrmsr/presto/TestConfig.java
+++ presto-wrmsr-extensions/src/test/java/com/wrmsr/presto/TestConfig.java
@@ -1,75 +1,28 @@
 package com.wrmsr.presto;
 
-import com.facebook.presto.server.SliceSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.DefaultDeserializationContext;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import com.wrmsr.presto.util.Files;
-import com.wrmsr.presto.util.ImmutableCollectors;
-import io.airlift.json.ObjectMapperProvider;
-import io.airlift.slice.Slice;
+import com.wrmsr.presto.util.Serialization;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationUtils;
 import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.apache.commons.configuration.MapConfiguration;
-import org.apache.commons.lang3.tuple.Pair;
 import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 public class TestConfig
 {
-    private static final Supplier<ObjectMapper> OBJECT_MAPPER = Suppliers.memoize(() ->
-            new ObjectMapperProvider().get()
-                    .registerModule(new SimpleModule().addSerializer(Slice.class, new SliceSerializer())));
-
-    @SuppressWarnings({"unchecked"})
-    private Map<String, String> translate(String key, Object value)
-    {
-        if (value == null) {
-            return ImmutableMap.of();
-        }
-        else if (value instanceof Map) {
-            Map<String, Object> mapValue = (Map<String, Object>) value;
-            return mapValue.entrySet().stream()
-                    .flatMap(e -> translate(key + "." + e.getKey(), e.getValue()).entrySet().stream())
-                    .collect(ImmutableCollectors.toImmutableMap(e -> e.getKey(), e -> e.getValue()));
-        }
-        else if (value instanceof List) {
-            List<Object> listValue = (List<Object>) value;
-            return IntStream.range(0, listValue.size()).boxed()
-                    .flatMap(i -> translate(key + "(" + i.toString() + ")", listValue.get(i)).entrySet().stream())
-                    .collect(ImmutableCollectors.toImmutableMap(e -> e.getKey(), e -> e.getValue()));
-        }
-        else {
-            return ImmutableMap.of(key, value.toString());
-        }
-    }
-
     @Test
     public void testStuff() throws Throwable
     {
-        String cfgStr = Files.readFile("/Users/wtimoney/yelp-presto.yaml");
+        String cfgStr = Files.readFile(System.getProperty("user.home") + "/presto/yelp-presto.yaml");
+        Serialization.splitYaml(cfgStr).get(0);
 
-        ObjectMapper omapper = OBJECT_MAPPER.get();
-        ObjectMapper mapper = new ObjectMapper(
-                new YAMLFactory(),
-                (DefaultSerializerProvider) omapper.getSerializerProvider(),
-                (DefaultDeserializationContext) omapper.getDeserializationContext()
-        );
-        Object o = mapper.readValue(cfgStr, Object.class);
+        Object o = Serialization.YAML_OBJECT_MAPPER.get().readValue(cfgStr, Object.class);
         System.out.println(o);
 
         Map<String, Object> m = (Map<String, Object>) o;
-        Map<String, String> t = translate("", o);
+        Map<String, String> t = Serialization.flattenYaml(null, o);
 
         Configuration c = new MapConfiguration(t);
         HierarchicalConfiguration hc = ConfigurationUtils.convertToHierarchical(c);
