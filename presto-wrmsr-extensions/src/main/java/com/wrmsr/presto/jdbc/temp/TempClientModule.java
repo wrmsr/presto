3b44f8f05e301d9ce8c540b84bcc04fb1d196d33
FUCKYOUGIT <FUCKYOUGIT@FUCKYOU.FUCK>
2015-06-06 15:29:13 -0700

    --

--- presto-wrmsr-extensions/src/main/java/com/wrmsr/presto/jdbc/temp/TempClientModule.java
+++ presto-wrmsr-extensions/src/main/java/com/wrmsr/presto/jdbc/temp/TempClientModule.java
@@ -15,7 +15,7 @@ public class TempClientModule
         return ImmutableMap.<String, String>builder()
                 .put("connection-url", format("jdbc:h2:mem:test%s;DB_CLOSE_DELAY=-1", System.nanoTime()))
                 .put("is-remotely-accessible", "false")
-                .put("init", "CREATE SCHEMA temp")
+                .put("init", "CREATE SCHEMA temp;")
                 .build();
     }
 }
