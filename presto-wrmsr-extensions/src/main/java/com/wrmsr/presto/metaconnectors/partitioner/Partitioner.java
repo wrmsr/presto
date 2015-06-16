5acfc58b9576c1b2c3a02da55c2f5089e68931c6
William Timoney <wtimoney@140-sfoengwifi53-229.clients.corp.yelpcorp.com>
2015-06-16 16:27:28 -0700

    --

--- presto-wrmsr-extensions/src/main/java/com/wrmsr/presto/metaconnectors/partitioner/Partitioner.java
+++ presto-wrmsr-extensions/src/main/java/com/wrmsr/presto/metaconnectors/partitioner/Partitioner.java
@@ -3,12 +3,19 @@ package com.wrmsr.presto.metaconnectors.partitioner;
 import com.facebook.presto.metadata.TableHandle;
 import com.facebook.presto.spi.ColumnHandle;
 import com.facebook.presto.spi.ConnectorPartition;
+import com.facebook.presto.spi.Domain;
+import com.facebook.presto.spi.Range;
 import com.facebook.presto.spi.SchemaTableName;
+import com.facebook.presto.spi.SortedRangeSet;
 import com.facebook.presto.spi.TupleDomain;
+import com.google.common.collect.ImmutableMap;
 import com.wrmsr.presto.util.ColumnDomain;
+import com.wrmsr.presto.util.ImmutableCollectors;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -42,6 +49,11 @@ public interface Partitioner
     static List<Partition> generateDensePartitions(LinkedHashMap<String, ColumnDomain> columnDomains, int numPartitions)
     {
         checkArgument(columnDomains.size() == 1); // FIXME
-        throw new UnsupportedOperationException();
+        Map.Entry<String, ColumnDomain> e = columnDomains.entrySet().stream().findFirst().get();
+        String column = e.getKey();
+        ColumnDomain columnDomain = e.getValue();
+        int base = (Integer) columnDomain.getMin();
+        int step = ((Integer) columnDomain.getMax() - base) / numPartitions;
+        return IntStream.range(0, numPartitions).boxed().map((i) -> new Partition(i.toString(), TupleDomain.withColumnDomains(ImmutableMap.of(column, Domain.create(SortedRangeSet.of(Range.range(i * step + base, true, (i + 1) * step + base, false)), false))))).collect(ImmutableCollectors.toImmutableList());
     }
 }
