583997a243433b6f8659eb7d38b58d3dbd92a9fd
FUCKYOUGIT <FUCKYOUGIT@FUCKYOU.FUCK>
2015-06-18 03:25:00 -0700

    --

--- presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperBuilder.java
+++ presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperBuilder.java
@@ -15,14 +15,18 @@ package com.wrmsr.presto.wrapper;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import io.airlift.log.Logger;
 import io.airlift.log.Logging;
 import io.airlift.resolver.ArtifactResolver;
-import io.airlift.log.Logger;
 import org.sonatype.aether.artifact.Artifact;
 
-import java.io.File;
+import java.io.*;
 import java.net.URL;
 import java.util.List;
+import java.util.jar.Attributes;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
 
 public class PrestoWrapperBuilder
 {
@@ -36,6 +40,50 @@ public class PrestoWrapperBuilder
             throws Throwable
     {
         new PrestoWrapperBuilder().run(args);
+    }
+
+    public void build() throws IOException
+    {
+        Manifest manifest = new Manifest();
+        manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
+        try (JarOutputStream target = new JarOutputStream(new FileOutputStream("output.jar"), manifest)) {
+            add(new File("inputDirectory"), target);
+        }
+    }
+
+    private void add(File source, JarOutputStream target) throws IOException
+    {
+        if (source.isDirectory()) {
+            String name = source.getPath().replace("\\", "/");
+            if (!name.isEmpty()) {
+                if (!name.endsWith("/")) {
+                    name += "/";
+                }
+                JarEntry entry = new JarEntry(name);
+                entry.setTime(source.lastModified());
+                target.putNextEntry(entry);
+                target.closeEntry();
+            }
+            for (File nestedFile : source.listFiles()) {
+                add(nestedFile, target);
+            }
+            return;
+        }
+
+        JarEntry entry = new JarEntry(source.getPath().replace("\\", "/"));
+        entry.setTime(source.lastModified());
+        target.putNextEntry(entry);
+        try (InputStream in = new BufferedInputStream(new FileInputStream(source))) {
+            byte[] buffer = new byte[1024];
+            while (true) {
+                int count = in.read(buffer);
+                if (count == -1) {
+                    break;
+                }
+                target.write(buffer, 0, count);
+            }
+            target.closeEntry();
+        }
     }
 
     public void run(String[] args)
