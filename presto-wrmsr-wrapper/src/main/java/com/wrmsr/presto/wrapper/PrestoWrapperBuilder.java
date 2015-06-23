71ed17af4a14bf0ae6d110d56594ca0cca660bd8
William Timoney <wtimoney@140-sfoengwifi53-229.clients.corp.yelpcorp.com>
2015-06-22 18:16:44 -0700

    --

--- presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperBuilder.java
+++ presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperBuilder.java
@@ -22,11 +22,18 @@ import org.sonatype.aether.artifact.Artifact;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
 import java.util.jar.Manifest;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
 
 public class PrestoWrapperBuilder
 {
@@ -40,6 +47,126 @@ public class PrestoWrapperBuilder
             throws Throwable
     {
         new PrestoWrapperBuilder().run(args);
+    }
+
+    public static abstract class Entry
+    {
+        private final String jarPath;
+
+        public Entry(String jarPath)
+        {
+            this.jarPath = jarPath;
+        }
+
+        public String getJarPath()
+        {
+            return jarPath;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Entry entry = (Entry) o;
+
+            return !(jarPath != null ? !jarPath.equals(entry.jarPath) : entry.jarPath != null);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return jarPath != null ? jarPath.hashCode() : 0;
+        }
+    }
+
+    public static class FileEntry extends Entry
+    {
+        private final File file;
+
+        public FileEntry(String jarPath, File file)
+        {
+            super(jarPath);
+            this.file = file;
+        }
+
+        public File getFile()
+        {
+            return file;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            if (!super.equals(o)) {
+                return false;
+            }
+
+            FileEntry fileEntry = (FileEntry) o;
+
+            return !(file != null ? !file.equals(fileEntry.file) : fileEntry.file != null);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            int result = super.hashCode();
+            result = 31 * result + (file != null ? file.hashCode() : 0);
+            return result;
+        }
+    }
+
+    public static class BytesEntry extends Entry
+    {
+        public final byte[] bytes;
+
+        public BytesEntry(String jarPath, byte[] bytes)
+        {
+            super(jarPath);
+            this.bytes = bytes;
+        }
+
+        public byte[] getBytes()
+        {
+            return bytes;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            if (!super.equals(o)) {
+                return false;
+            }
+
+            BytesEntry that = (BytesEntry) o;
+
+            return Arrays.equals(bytes, that.bytes);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            int result = super.hashCode();
+            result = 31 * result + (bytes != null ? Arrays.hashCode(bytes) : 0);
+            return result;
+        }
     }
 
     public void build() throws IOException
@@ -91,45 +218,78 @@ public class PrestoWrapperBuilder
     {
         Logging.initialize();
 
+        File jarRepoBase = new File("/repository");
+        File classpathBase = new File("/classpaths");
+        File repository = new File(System.getProperty("user.home"), ".m2/repository");
         File cwd = new File(System.getProperty("user.dir"));
 
         ArtifactResolver resolver = new ArtifactResolver(
                 ArtifactResolver.USER_LOCAL_REPO,
                 ImmutableList.of(ArtifactResolver.MAVEN_CENTRAL_URI));
 
-        List<String> poms = ImmutableList.of(
-                "presto-main/pom.xml",
-                "presto-cassandra/pom.xml",
-                "presto-example-http/pom.xml",
-                "presto-hive-hadoop2/pom.xml",
-                "presto-kafka/pom.xml",
-                "presto-ml/pom.xml",
-                "presto-mysql/pom.xml",
-                "presto-postgresql/pom.xml",
-                "presto-raptor/pom.xml",
-                "presto-tpch/pom.xml",
-                "presto-wrmsr-extensions/pom.xml"
+        List<String> names = ImmutableList.of(
+                "presto-main",
+                "presto-cassandra",
+                "presto-blackhole",
+                "presto-example-http",
+                "presto-hive-hadoop2",
+                "presto-kafka",
+                "presto-ml",
+                "presto-mysql",
+                "presto-postgresql",
+                "presto-raptor",
+                "presto-tpch",
+                "presto-wrmsr-extensions",
+                "presto-wrmsr-wrapper" // BOOTSTRAP SELF
         );
 
-        for (String pom : poms) {
-            log.info(pom);
+        Set<Entry> entries = newHashSet();
+        for (String name : names) {
+            String pom = name + "/pom.xml";
+            // log.info(pom);
 
-            List<Artifact> artifacts = resolver.resolvePom(new File(cwd, pom));
+            List<String> repoPaths = newArrayList();
+            File pomFile = new File(cwd, pom);
+            List<Artifact> artifacts = resolver.resolvePom(pomFile);
 
-            List<File> files = Lists.newArrayList();
+            List<File> files = newArrayList();
             for (Artifact a : artifacts) {
                 files.add(a.getFile());
             }
 
             for (File file : files) {
-                if (!file.exists()) {
-                    // throw new IllegalStateException(file.getAbsolutePath());
-                    log.warn(String.format("File not found: %s", file));
-                }
+                checkState(file.exists(), String.format("File not found: %s", file));
                 URL url = new URL("file:" + file.getAbsolutePath() + (file.isDirectory() ? "/" : ""));
-                log.info(url.toString());
+                //log.info(url.toString());
+                if (file.isDirectory()) {
+                    // FIXME translate target/classes to jar
+                    log.info(file.toString());
+                }
+                else {
+                    String rel = repository.toURI().relativize(file.toURI()).getPath();
+                    checkState(!rel.startsWith("/") && !rel.startsWith(".."));
+                    //log.info(rel);
+
+                    File jarPath = new File(jarRepoBase, rel);
+                    String jarPathStr = jarPath.toString();
+                    checkState(jarPathStr.startsWith("/"));
+                    jarPathStr = jarPathStr.substring(1);
+
+                    entries.add(new FileEntry(jarPathStr, file));
+
+                    //log.info(jarPathStr);
+                    repoPaths.add(jarPathStr);
+                }
             }
+
+            String repoPathsStr = String.join("\n", repoPaths) + "\n";
+            String classpathPath = new File(classpathBase, name).toString();
+            checkState(classpathPath.startsWith("/"));
+            classpathPath = classpathPath.substring(1);
+            entries.add(new BytesEntry(classpathPath, repoPathsStr.getBytes()));
         }
+
+        System.out.println(entries);
     }
 }
 
