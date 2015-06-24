b37442b4c0050151b7ea2f8e31d117f9836f3866
FUCKYOUGIT <FUCKYOUGIT@FUCKYOU.FUCK>
2015-06-24 08:12:30 -0700

    --

---
+++ presto-wrmsr-wrapper/list_jdks.sh
@@ -0,0 +1,4 @@
+find /Library/Java/JavaVirtualMachines/ -maxdepth 1 -type d -not -path /Library/Java/JavaVirtualMachines/
+find /usr/lib/jvm/ -maxdepth 1 -type d -not -path /usr/lib/jvm/
+find ~ -maxdepth 1 -type d -not -path ~
+
