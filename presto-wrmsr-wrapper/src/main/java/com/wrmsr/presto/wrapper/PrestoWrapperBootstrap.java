91d4077fda31c58040e237f15f35f6117c1e402e
William Timoney <wtimoney@140-sfoengwifi53-229.clients.corp.yelpcorp.com>
2015-06-25 19:27:50 -0700

    --

--- presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperBootstrap.java
+++ presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperBootstrap.java
@@ -1,15 +1,27 @@
 package com.wrmsr.presto.wrapper;
 
-import java.io.File;
 import java.util.Scanner;
 
 public class PrestoWrapperBootstrap
 {
+    public static class NestedClassloader extends ClassLoader
+    {
+        public NestedClassloader(ClassLoader parent)
+        {
+            super(parent);
+        }
+
+        public NestedClassloader()
+        {
+        }
+
+
+    }
+
     public static void main(String[] args) throws Throwable
     {
         ClassLoader classLoader = PrestoWrapperBootstrap.class.getClassLoader();
-        File file = new File(classLoader.getResource("classpaths/presto-wrmsr-wrapper").getFile());
-        try (Scanner scanner = new Scanner(file)) {
+        try (Scanner scanner = new Scanner(classLoader.getResourceAsStream("classpaths/presto-wrmsr-wrapper"))) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 System.out.println(line);
