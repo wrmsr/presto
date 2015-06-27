e5ae7089f654d3056177ab9e21ea9ceabb7fea58
FUCKYOUGIT <FUCKYOUGIT@FUCKYOU.FUCK>
2015-06-27 07:58:15 -0700

    --

--- presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperMain.java
+++ presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/wrapper/PrestoWrapperMain.java
@@ -19,6 +19,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.wrmsr.presto.util.Repositories;
 import com.wrmsr.presto.wrapper.util.ParentLastURLClassLoader;
+import io.airlift.command.*;
 import io.airlift.resolver.ArtifactResolver;
 import io.airlift.log.Logger;
 import org.sonatype.aether.artifact.Artifact;
@@ -32,6 +33,64 @@ import java.util.Map;
 
 public class PrestoWrapperMain
 {
+    public static void main2(String[] args)
+    {
+        Cli.CliBuilder<Runnable> builder = Cli.<Runnable>builder("git")
+                .withDescription("the stupid content tracker")
+                .withDefaultCommand(Help.class)
+                .withCommands(Help.class, Add.class);
+
+        builder.withGroup("remote")
+                .withDescription("Manage set of tracked repositories")
+                .withDefaultCommand(RemoteShow.class)
+                .withCommands(RemoteShow.class, RemoteAdd.class);
+
+        Cli<Runnable> gitParser = builder.build();
+
+        gitParser.parse(args).run();
+    }
+
+    public static class GitCommand implements Runnable
+    {
+        @Option(type = OptionType.GLOBAL, name = "-v", description = "Verbose mode")
+        public boolean verbose;
+
+        public void run()
+        {
+            System.out.println(getClass().getSimpleName());
+        }
+    }
+
+    @Command(name = "add", description = "Add file contents to the index")
+    public static class Add extends GitCommand
+    {
+        @Arguments(description = "Patterns of files to be added")
+        public List<String> patterns;
+
+        @Option(name = "-i", description = "Add modified contents interactively.")
+        public boolean interactive;
+    }
+
+    @Command(name = "show", description = "Gives some information about the remote <name>")
+    public static class RemoteShow extends GitCommand
+    {
+        @Option(name = "-n", description = "Do not query remote heads")
+        public boolean noQuery;
+
+        @Arguments(description = "Remote to show")
+        public String remote;
+    }
+
+    @Command(name = "add", description = "Adds a remote")
+    public static class RemoteAdd extends GitCommand
+    {
+        @Option(name = "-t", description = "Track only a specific branch")
+        public String branch;
+
+        @Arguments(description = "Remote repository to add")
+        public List<String> remote;
+    }
+
     private PrestoWrapperMain()
     {
     }
