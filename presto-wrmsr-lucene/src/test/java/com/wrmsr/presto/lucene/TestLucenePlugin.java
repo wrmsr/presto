7f1c3f02876a9d4b1e1222b95aa35fb3a33ea60f
William Timoney <wtimoney@140-sfoengwifi53-229.clients.corp.yelpcorp.com>
2015-06-30 17:32:22 -0700

    --

---
+++ presto-wrmsr-lucene/src/test/java/com/wrmsr/presto/lucene/TestLucenePlugin.java
@@ -0,0 +1,30 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wrmsr.presto.lucene;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.script.SimpleBindings;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestLucenePlugin
+{
+    @Test
+    public void testStuff() throws Throwable
+    {
+    }
+}
