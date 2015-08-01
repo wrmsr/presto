14ab5cd447c1b7b79a2c07d4c7f525442395a0ad
William Timoney <wtimoney@140-sfoengwifi54-85.clients.corp.yelpcorp.com>
2015-07-31 17:58:58 -0700

    --

---
+++ presto-wrmsr-extensions/src/main/java/com/wrmsr/presto/reactor/InsertRewriter.java
@@ -0,0 +1,15 @@
+package com.wrmsr.presto.reactor;
+
+import com.facebook.presto.sql.analyzer.Analysis;
+import com.facebook.presto.sql.tree.DefaultTraversalVisitor;
+
+public class InsertRewriter
+    extends DefaultTraversalVisitor<Void, Void>
+{
+    private final Analysis analysis;
+
+    public InsertRewriter(Analysis analysis)
+    {
+        this.analysis = analysis;
+    }
+}
---
+++ presto-wrmsr-extensions/src/test/java/com/wrmsr/presto/reactor/TestInsertRewriter.java
@@ -0,0 +1,159 @@
+package com.wrmsr.presto.reactor;
+
+import com.facebook.presto.Session;
+import com.facebook.presto.block.BlockEncodingManager;
+import com.facebook.presto.metadata.MetadataManager;
+import com.facebook.presto.metadata.QualifiedTableName;
+import com.facebook.presto.metadata.TableMetadata;
+import com.facebook.presto.metadata.ViewDefinition;
+import com.facebook.presto.spi.ColumnMetadata;
+import com.facebook.presto.spi.ConnectorTableMetadata;
+import com.facebook.presto.spi.SchemaTableName;
+import com.facebook.presto.spi.type.TypeManager;
+import com.facebook.presto.split.SplitManager;
+import com.facebook.presto.sql.analyzer.Analysis;
+import com.facebook.presto.sql.analyzer.Analyzer;
+import com.facebook.presto.sql.analyzer.FeaturesConfig;
+import com.facebook.presto.sql.analyzer.Field;
+import com.facebook.presto.sql.analyzer.SemanticErrorCode;
+import com.facebook.presto.sql.analyzer.SemanticException;
+import com.facebook.presto.sql.parser.SqlParser;
+import com.facebook.presto.sql.tree.AllColumns;
+import com.facebook.presto.sql.tree.Insert;
+import com.facebook.presto.sql.tree.QualifiedName;
+import com.facebook.presto.sql.tree.Query;
+import com.facebook.presto.sql.tree.QuerySpecification;
+import com.facebook.presto.sql.tree.Statement;
+import com.facebook.presto.sql.tree.Table;
+import com.facebook.presto.type.TypeRegistry;
+import com.google.common.collect.ImmutableList;
+import io.airlift.json.JsonCodec;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.intellij.lang.annotations.Language;
+
+import java.util.Optional;
+
+import static com.facebook.presto.spi.type.BigintType.BIGINT;
+import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
+import static com.facebook.presto.sql.QueryUtil.selectList;
+import static com.facebook.presto.sql.QueryUtil.simpleQuery;
+import static com.facebook.presto.sql.QueryUtil.table;
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Locale.ENGLISH;
+import static com.facebook.presto.spi.type.TimeZoneKey.UTC_KEY;
+import static org.testng.Assert.fail;
+import static java.lang.String.format;
+
+public class TestInsertRewriter
+{
+    public static final Session SESSION = Session.builder()
+            .setUser("user")
+            .setSource("test")
+            .setCatalog("default")
+            .setSchema("default")
+            .setTimeZoneKey(UTC_KEY)
+            .setLocale(ENGLISH)
+            .build();
+
+    private static final SqlParser SQL_PARSER = new SqlParser();
+
+    private Analyzer analyzer;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setup()
+            throws Exception
+    {
+        TypeManager typeManager = new TypeRegistry();
+        MetadataManager metadata = new MetadataManager(new FeaturesConfig().setExperimentalSyntaxEnabled(true), typeManager, new SplitManager(), new BlockEncodingManager(typeManager));
+        metadata.addConnectorMetadata("tpch", "tpch", new TestingMetadata());
+        metadata.addConnectorMetadata("c2", "c2", new TestingMetadata());
+        metadata.addConnectorMetadata("c3", "c3", new TestingMetadata());
+
+        SchemaTableName table1 = new SchemaTableName("default", "t1");
+        metadata.createTable(SESSION, "tpch", new TableMetadata("tpch", new ConnectorTableMetadata(table1,
+                ImmutableList.<ColumnMetadata>of(
+                        new ColumnMetadata("a", BIGINT, false),
+                        new ColumnMetadata("b", BIGINT, false),
+                        new ColumnMetadata("c", BIGINT, false),
+                        new ColumnMetadata("d", BIGINT, false)))));
+
+        SchemaTableName table2 = new SchemaTableName("default", "t2");
+        metadata.createTable(SESSION, "tpch", new TableMetadata("tpch", new ConnectorTableMetadata(table2,
+                ImmutableList.<ColumnMetadata>of(
+                        new ColumnMetadata("a", BIGINT, false),
+                        new ColumnMetadata("b", BIGINT, false)))));
+
+        SchemaTableName table3 = new SchemaTableName("default", "t3");
+        metadata.createTable(SESSION, "tpch", new TableMetadata("tpch", new ConnectorTableMetadata(table3,
+                ImmutableList.<ColumnMetadata>of(
+                        new ColumnMetadata("a", BIGINT, false),
+                        new ColumnMetadata("b", BIGINT, false),
+                        new ColumnMetadata("x", BIGINT, false, null, true)))));
+
+        // table in different catalog
+        SchemaTableName table4 = new SchemaTableName("s2", "t4");
+        metadata.createTable(SESSION, "c2", new TableMetadata("tpch", new ConnectorTableMetadata(table4,
+                ImmutableList.<ColumnMetadata>of(
+                        new ColumnMetadata("a", BIGINT, false)))));
+
+        // table with a hidden column
+        SchemaTableName table5 = new SchemaTableName("default", "t5");
+        metadata.createTable(SESSION, "tpch", new TableMetadata("tpch", new ConnectorTableMetadata(table5,
+                ImmutableList.<ColumnMetadata>of(
+                        new ColumnMetadata("a", BIGINT, false),
+                        new ColumnMetadata("b", BIGINT, false, null, true)))));
+
+        // valid view referencing table in same schema
+        String viewData1 = JsonCodec.jsonCodec(ViewDefinition.class).toJson(
+                new ViewDefinition("select a from t1", "tpch", "default", ImmutableList.of(
+                        new ViewDefinition.ViewColumn("a", BIGINT))));
+        metadata.createView(SESSION, new QualifiedTableName("tpch", "default", "v1"), viewData1, false);
+
+        // stale view (different column type)
+        String viewData2 = JsonCodec.jsonCodec(ViewDefinition.class).toJson(
+                new ViewDefinition("select a from t1", "tpch", "default", ImmutableList.of(
+                        new ViewDefinition.ViewColumn("a", VARCHAR))));
+        metadata.createView(SESSION, new QualifiedTableName("tpch", "default", "v2"), viewData2, false);
+
+        // view referencing table in different schema from itself and session
+        String viewData3 = JsonCodec.jsonCodec(ViewDefinition.class).toJson(
+                new ViewDefinition("select a from t4", "c2", "s2", ImmutableList.of(
+                        new ViewDefinition.ViewColumn("a", BIGINT))));
+        metadata.createView(SESSION, new QualifiedTableName("c3", "s3", "v3"), viewData3, false);
+
+        analyzer = new Analyzer(
+                Session.builder()
+                        .setUser("user")
+                        .setSource("test")
+                        .setCatalog("tpch")
+                        .setSchema("default")
+                        .setTimeZoneKey(UTC_KEY)
+                        .setLocale(ENGLISH)
+                        .build(),
+                metadata,
+                SQL_PARSER,
+                Optional.empty(),
+                true);
+    }
+
+    @Test
+    public void testShit() throws Throwable
+    {
+        @Language("SQL") String queryStr = "select a, b from t1";
+
+        Statement statement = SQL_PARSER.createStatement(queryStr);
+        Analysis analysis = analyzer.analyze(statement);
+
+        InsertRewriter rewriter = new InsertRewriter(analysis);
+        rewriter.process(statement, null);
+
+        Field pk = analysis.getOutputDescriptor().getFieldByIndex(0);
+        Query query = analysis.getQuery();
+        QuerySpecification querySpecification = (QuerySpecification) query.getQueryBody();
+        Table table =  (Table) querySpecification.getFrom().get();
+
+        Insert insert = new Insert(QualifiedName.of("t2"), query);
+        Analysis insertAnalysis = analyzer.analyze(insert);
+    }
+}
