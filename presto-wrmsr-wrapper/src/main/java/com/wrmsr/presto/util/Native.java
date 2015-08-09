9bdffa7906b52dd3dce08655deab0c24a08b938f
FUCKYOUGIT <FUCKYOUGIT@FUCKYOU.FUCK>
2015-08-09 03:35:36 -0700

    --

--- presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/util/Native.java
+++ presto-wrmsr-wrapper/src/main/java/com/wrmsr/presto/util/Native.java
@@ -1,1 +1,1 @@
-../../../../../../../../presto-wrmsr-extensions/src/main/java/com/wrmsr/presto/util/Native.java
+../../../../../../../../presto-wrmsr-main/src/main/java/com/wrmsr/presto/util/Native.java
