bbf44c10327c08e5b8b21da4e75d84ee17200202
FUCKYOUGIT <FUCKYOUGIT@FUCKYOU.FUCK>
2015-08-16 01:05:40 -0700

    --

--- presto-wrmsr-launcher/src/main/java/com/wrmsr/presto/launcher/cluster/Config.java
+++ presto-wrmsr-launcher/src/main/java/com/wrmsr/presto/launcher/cluster/Config.java
@@ -1,5 +1,8 @@
 package com.wrmsr.presto.launcher.cluster;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.File;
 import java.util.List;
 import java.util.Map;
@@ -10,11 +13,35 @@ public class Config
     {
         private final String name;
         private final List<Node> nodes;
+        private final Node defaults;
 
-        public Cluster(String name, List<Node> nodes)
+        @JsonCreator
+        public Cluster(
+                @JsonProperty("name") String name,
+                @JsonProperty("nodes") List<Node> nodes,
+                @JsonProperty("defaults") Node defaults)
         {
             this.name = name;
             this.nodes = nodes;
+            this.defaults = defaults;
+        }
+
+        @JsonProperty
+        public String getName()
+        {
+            return name;
+        }
+
+        @JsonProperty
+        public List<Node> getNodes()
+        {
+            return nodes;
+        }
+
+        @JsonProperty
+        public Node getDefaults()
+        {
+            return defaults;
         }
     }
 
@@ -26,13 +53,49 @@ public class Config
         private final File identity;
         private final Map<String, Integer> ports;
 
-        public Node(String host, String username, String password, File identity, Map<String, Integer> ports)
+        @JsonCreator
+        public Node(
+                @JsonProperty("host") String host,
+                @JsonProperty("username") String username,
+                @JsonProperty("password") String password,
+                @JsonProperty("identity") File identity,
+                @JsonProperty("ports") Map<String, Integer> ports)
         {
             this.host = host;
             this.username = username;
             this.password = password;
             this.identity = identity;
             this.ports = ports;
+        }
+
+        @JsonProperty
+        public String getHost()
+        {
+            return host;
+        }
+
+        @JsonProperty
+        public String getUsername()
+        {
+            return username;
+        }
+
+        @JsonProperty
+        public String getPassword()
+        {
+            return password;
+        }
+
+        @JsonProperty
+        public File getIdentity()
+        {
+            return identity;
+        }
+
+        @JsonProperty
+        public Map<String, Integer> getPorts()
+        {
+            return ports;
         }
     }
 }
