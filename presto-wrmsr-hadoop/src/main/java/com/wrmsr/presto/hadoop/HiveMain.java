1b41af973ee53fce601d3a86b78ce5b4bee5dac4
William Timoney <wtimoney@140-sfoengwifi54-85.clients.corp.yelpcorp.com>
2015-08-25 15:21:23 -0700

    --

--- presto-wrmsr-hadoop/src/main/java/com/wrmsr/presto/hadoop/HiveMain.java
+++ presto-wrmsr-hadoop/src/main/java/com/wrmsr/presto/hadoop/HiveMain.java
@@ -50,9 +50,9 @@ public class HiveMain
         public void run()
         {
             try {
-
+                runNothrow();
             }
-            catch (Exception e) {
+            catch (Throwable e) {
                 throw Throwables.propagate(e);
             }
         }
