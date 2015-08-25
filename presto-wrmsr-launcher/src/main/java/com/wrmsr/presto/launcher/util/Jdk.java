da2cef937920c2e4c1b5cbaad1851234c84187a5
William Timoney <wtimoney@140-sfoengwifi54-85.clients.corp.yelpcorp.com>
2015-08-25 09:03:09 -0700

    --

--- presto-wrmsr-launcher/src/main/java/com/wrmsr/presto/launcher/util/Jdk.java
+++ presto-wrmsr-launcher/src/main/java/com/wrmsr/presto/launcher/util/Jdk.java
@@ -17,8 +17,8 @@ public class Jdk
       --no-cookies \
       --no-check-certificate \
       --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-      "https://download.oracle.com/otn-pub/java/jdk/8u51-b16/jdk-8u51-linux-x64.tar.gz" \
-      --output-document="jdk-8u51-linux-x64.tar.gz"
+      "https://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.tar.gz" \
+      --output-document="jdk-8u60-linux-x64.tar.gz"
     */
 
     /**
