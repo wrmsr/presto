1648fa400b330833c05580526f86e974319b694b
wrmsr <timwilloney@gmail.com>
2015-08-27 10:32:47 -0700

    --

--- presto-wrmsr-main/src/test/java/com/wrmsr/presto/functions/TestGrokFunctions.java
+++ presto-wrmsr-main/src/test/java/com/wrmsr/presto/functions/TestGrokFunctions.java
@@ -22,7 +22,7 @@ public class TestGrokFunctions
     @Test
     public void testStuff() throws Throwable
     {
-        Grok grok = Grok.create("patterns/patterns");
+        Grok grok = Grok.create("grok-patterns/grok-patterns");
         grok.compile("%{COMBINEDAPACHELOG}");
         String log = "112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET / HTTP/1.1\" 200 44346 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"";
         Match gm = grok.match(log);
