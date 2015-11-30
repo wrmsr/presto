7f9aa44f5ed0f3db4eb0e3a6de2284036a7e9580
wrmsr <timwilloney@gmail.com>
2015-11-29 17:55:21 -0800

    --

--- presto-wrmsr-main/src/main/java/com/wrmsr/presto/scripting/ScriptFunction.java
+++ presto-wrmsr-main/src/main/java/com/wrmsr/presto/scripting/ScriptFunction.java
@@ -1,21 +1,47 @@
 package com.wrmsr.presto.scripting;
 
+import com.facebook.presto.byteCode.ByteCodeBlock;
+import com.facebook.presto.byteCode.ClassDefinition;
+import com.facebook.presto.byteCode.DynamicClassLoader;
+import com.facebook.presto.byteCode.MethodDefinition;
+import com.facebook.presto.byteCode.Parameter;
+import com.facebook.presto.byteCode.Scope;
+import com.facebook.presto.byteCode.Variable;
 import com.facebook.presto.metadata.FunctionRegistry;
 import com.facebook.presto.metadata.SqlScalarFunction;
 import com.facebook.presto.metadata.TypeParameter;
+import com.facebook.presto.operator.scalar.ScalarFunction;
 import com.facebook.presto.operator.scalar.ScalarFunctionImplementation;
+import com.facebook.presto.spi.Connector;
 import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.block.BlockBuilder;
+import com.facebook.presto.spi.block.BlockBuilderStatus;
+import com.facebook.presto.spi.block.VariableWidthBlockBuilder;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.spi.type.TypeManager;
 import com.facebook.presto.spi.type.VarcharType;
+import com.facebook.presto.sql.gen.CallSiteBinder;
+import com.facebook.presto.sql.gen.CompilerUtils;
+import com.facebook.presto.type.SqlType;
 import com.google.common.collect.ImmutableList;
+import com.wrmsr.presto.functions.StructManager;
 import io.airlift.slice.Slice;
+import org.apache.commons.lang3.ClassUtils;
 
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 
+import static com.facebook.presto.byteCode.Access.FINAL;
+import static com.facebook.presto.byteCode.Access.PRIVATE;
+import static com.facebook.presto.byteCode.Access.PUBLIC;
+import static com.facebook.presto.byteCode.Access.STATIC;
+import static com.facebook.presto.byteCode.Access.a;
+import static com.facebook.presto.byteCode.Parameter.arg;
+import static com.facebook.presto.byteCode.ParameterizedType.type;
+import static com.facebook.presto.sql.gen.CompilerUtils.defineClass;
 import static com.facebook.presto.util.Reflection.methodHandle;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.wrmsr.presto.util.Serialization.OBJECT_MAPPER;
@@ -86,14 +112,92 @@ public class ScriptFunction
     public ScalarFunctionImplementation specialize(Map<String, Type> types, int arity, TypeManager typeManager, FunctionRegistry functionRegistry)
     {
         checkArgument(arity == this.arity + 2);
+
+        String name = "script_invoker";
+
+        ClassDefinition definition = new ClassDefinition(
+                a(PUBLIC, FINAL),
+                CompilerUtils.makeClassName(name),
+                type(Object.class));
+
+        definition.declareDefaultConstructor(a(PRIVATE));
+
+        ImmutableList.Builder<Parameter> parametersBuilder = ImmutableList.builder();
+        parametersBuilder.add(arg("context", Context.class));
+        parametersBuilder.add(arg("session", ConnectorSession.class));
+        parametersBuilder.add(arg("scriptingName", Slice.class));
+        parametersBuilder.add(arg("functionName", Slice.class));
+        for (int i = 0; i < arity - 2; i++) {
+            Type argType = types.get("T" + i);
+            Class<?> javaType = argType.getJavaType();
+            // javaType = ClassUtils.primitiveToWrapper(javaType);
+            parametersBuilder.add(arg("arg" + i, javaType));
+        }
+        List<Parameter> parameters = parametersBuilder.build();
+
+        MethodDefinition methodDefinition = definition.declareMethod(a(PUBLIC, STATIC), name, type(com.facebook.presto.spi.block.Block.class), parameters);
+        methodDefinition.declareAnnotation(ScalarFunction.class);
+        methodDefinition.declareAnnotation(SqlType.class).setValue("value", retType.getTypeSignature().toString());
+        methodDefinition.declareParameterAnnotation(SqlType.class, 2).setValue("value", "varchar");
+        methodDefinition.declareParameterAnnotation(SqlType.class, 3).setValue("value", "varchar");
+        for (int i = 0; i < arity - 2; i++) {
+            methodDefinition.declareParameterAnnotation(SqlType.class, i + 4).setValue("value", types.get("T" + i).getTypeSignature().toString());
+        }
+
+        Scope scope = methodDefinition.getScope();
+        CallSiteBinder binder = new CallSiteBinder();
+        ByteCodeBlock body = methodDefinition.getBody();
+
+        body
+                .getVariable(scope.getVariable("context"))
+                .getVariable(scope.getVariable("session"))
+                .getVariable(scope.getVariable("scriptingName"))
+                .getVariable(scope.getVariable("functionName"))
+                .push(arity - 2)
+                .newArray(Object.class);
+
+        for (int i = 0; i < arity - 2; i++) {
+            Variable arg = scope.getVariable("arg" + i);
+            body
+                    .dup()
+                    .push(i)
+                    .getVariable(arg);
+
+            Type argType = types.get("T" + i);
+            Class<?> javaType = argType.getJavaType();
+
+            if (javaType == boolean.class) {
+                body.invokeStatic(Boolean.class, "valueOf", Boolean.class, boolean.class);
+            }
+            else if (javaType == long.class) {
+                body.invokeStatic(Long.class, "valueOf", Long.class, long.class);
+            }
+            else if (javaType == double.class) {
+                body.invokeStatic(Double.class, "valueOf", Double.class, double.class);
+            }
+
+            body.putObjectArrayElement();
+        }
+
+        body
+                .invokeStatic(ScriptFunction.class, "script", Context.class, ConnectorSession.class, Slice.class, Slice.class, Object[].class)
+                .retObject();
+
+        Class<?> cls = defineClass(definition, Object.class, binder.getBindings(), new DynamicClassLoader(StructManager.RowTypeConstructorCompiler.class.getClassLoader()));
+
+        /*
         List<Type> argTypes = ImmutableList.<Type>builder()
                 .add(VarcharType.VARCHAR)
                 .add(VarcharType.VARCHAR)
                 .addAll(IntStream.range(0, arity - 2).boxed().map(n -> types.get("T" + n.toString())).collect(toImmutableList()))
                 .build();
+
         MethodHandle methodHandle = METHOD_HANDLE.bindTo(new Context(scriptingManager, retType, argTypes)).asVarargsCollector(Object[].class);
         // FIXME nullable controls
         return new ScalarFunctionImplementation(true, listOf(arity, true), methodHandle, isDeterministic());
+        */
+
+        throw new IllegalStateException();
     }
 
     public static Object script(Context context, ConnectorSession session, Slice scriptingName, Slice functionName, Object... args)
--- presto-wrmsr-main/src/test/java/com/wrmsr/presto/scripting/TestScriptFunction.java
+++ presto-wrmsr-main/src/test/java/com/wrmsr/presto/scripting/TestScriptFunction.java
@@ -14,49 +14,23 @@
 package com.wrmsr.presto.scripting;
 
 import com.facebook.presto.Session;
-import com.facebook.presto.byteCode.ClassDefinition;
-import com.facebook.presto.byteCode.DynamicClassLoader;
-import com.facebook.presto.byteCode.MethodDefinition;
-import com.facebook.presto.byteCode.Parameter;
-import com.facebook.presto.byteCode.Scope;
-import com.facebook.presto.byteCode.Variable;
-import com.facebook.presto.metadata.FunctionFactory;
 import com.facebook.presto.metadata.SqlFunction;
-import com.facebook.presto.operator.scalar.ScalarFunction;
-import com.facebook.presto.spi.block.BlockBuilder;
-import com.facebook.presto.spi.block.BlockBuilderStatus;
-import com.facebook.presto.spi.block.VariableWidthBlockBuilder;
-import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.spi.type.VarcharType;
-import com.facebook.presto.sql.gen.CallSiteBinder;
-import com.facebook.presto.sql.gen.CompilerUtils;
 import com.facebook.presto.testing.LocalQueryRunner;
-import com.facebook.presto.testing.QueryRunner;
 import com.facebook.presto.tests.AbstractTestQueryFramework;
 import com.facebook.presto.tpch.TpchConnectorFactory;
-import com.facebook.presto.type.ParametricType;
-import com.facebook.presto.type.SqlType;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Iterables;
-import io.airlift.slice.Slice;
 import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static com.facebook.presto.byteCode.Access.FINAL;
-import static com.facebook.presto.byteCode.Access.PRIVATE;
-import static com.facebook.presto.byteCode.Access.PUBLIC;
-import static com.facebook.presto.byteCode.Access.STATIC;
-import static com.facebook.presto.byteCode.Access.a;
-import static com.facebook.presto.byteCode.ParameterizedType.type;
-import static com.facebook.presto.sql.gen.CompilerUtils.defineClass;
 import static com.facebook.presto.testing.TestingSession.testSessionBuilder;
 import static com.facebook.presto.tpch.TpchMetadata.TINY_SCHEMA_NAME;
 import static com.wrmsr.presto.util.collect.ImmutableCollectors.toImmutableList;
 
 public class TestScriptFunction
-    extends AbstractTestQueryFramework
+        extends AbstractTestQueryFramework
 {
 
     public TestScriptFunction()
@@ -65,79 +39,12 @@ public class TestScriptFunction
     }
 
     @Test
-    public void testStuff() throws Throwable
+    public void testStuff()
+            throws Throwable
     {
         LocalQueryRunner runner = createLocalQueryRunner();
 
         assertQuery("select script('a', 'b', 1, 'two')", "select null");
-    }
-
-    private buildThunk()
-    {
-        ClassDefinition definition = new ClassDefinition(
-                a(PUBLIC, FINAL),
-                CompilerUtils.makeClassName(rowType.getDisplayName() + "_new"),
-                type(Object.class));
-
-        definition.declareDefaultConstructor(a(PRIVATE));
-
-        List<Parameter> parameters = createParameters(fieldTypes);
-
-        MethodDefinition methodDefinition = definition.declareMethod(a(PUBLIC, STATIC), name, type(com.facebook.presto.spi.block.Block.class), parameters);
-        methodDefinition.declareAnnotation(ScalarFunction.class);
-        methodDefinition.declareAnnotation(SqlType.class).setValue("value", rowType.getTypeSignature().toString());
-        for (int i = 0; i < fieldTypes.size(); i++) {
-            methodDefinition.declareParameterAnnotation(SqlType.class, i).setValue("value", fieldTypes.get(i).getType().toString());
-        }
-
-        Scope scope = methodDefinition.getScope();
-        CallSiteBinder binder = new CallSiteBinder();
-        com.facebook.presto.byteCode.ByteCodeBlock body = methodDefinition.getBody();
-
-        Variable blockBuilder = scope.declareVariable(BlockBuilder.class, "blockBuilder");
-
-        body
-                .newObject(type(VariableWidthBlockBuilder.class, BlockBuilderStatus.class))
-                .dup()
-                .dup()
-                .newObject(BlockBuilderStatus.class)
-                .dup()
-                .invokeConstructor(BlockBuilderStatus.class)
-                .invokeConstructor(VariableWidthBlockBuilder.class, BlockBuilderStatus.class)
-                .putVariable(blockBuilder);
-
-        // FIXME: reuse returned blockBuilder
-
-        for (int i = 0; i < fieldTypes.size(); i++) {
-            Variable arg = scope.getVariable("arg" + i);
-            Class<?> javaType = fieldTypes.get(i).getType().getJavaType();
-
-            if (javaType == boolean.class) {
-                writeBoolean(body, blockBuilder, arg, i);
-            }
-            else if (javaType == long.class) {
-                writeLong(body, blockBuilder, arg, i);
-            }
-            else if (javaType == double.class) {
-                writeDouble(body, blockBuilder, arg, i);
-            }
-            else if (javaType == Slice.class) {
-                writeSlice(body, blockBuilder, arg, i);
-            }
-            else if (javaType == com.facebook.presto.spi.block.Block.class) {
-                writeObject(body, blockBuilder, arg, i);
-            }
-            else {
-                throw new IllegalArgumentException("bad value: " + javaType);
-            }
-        }
-
-        body
-                .getVariable(blockBuilder)
-                .invokeInterface(BlockBuilder.class, "build", com.facebook.presto.spi.block.Block.class)
-                .retObject();
-
-        return defineClass(definition, Object.class, binder.getBindings(), new DynamicClassLoader(RowTypeConstructorCompiler.class.getClassLoader()));
     }
 
     private static LocalQueryRunner createLocalQueryRunner()
