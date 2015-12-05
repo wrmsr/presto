e045b05a287b9113a5a5ce58155994f8f069b916
wrmsr <timwilloney@gmail.com>
2015-12-04 19:56:26 -0800

    --

---
+++ presto-wrmsr-main/src/main/java/com/wrmsr/presto/statistics/Statistics.java
@@ -0,0 +1,14 @@
+package com.wrmsr.presto.statistics;
+
+/*
+type inference
+structure inference
+min/max *
+mean/median/mode
+cardinality estimation
+guideposts
+*/
+
+public class Statistics
+{
+}
