fce39f49f450b09b311f5b81397356a062494aed
wrmsr <timwilloney@gmail.com>
2015-12-17 17:30:33 -0800

    --

--- presto-wrmsr-launcher/src/main/java/com/wrmsr/presto/launcher/util/DaemonProcess.java
+++ presto-wrmsr-launcher/src/main/java/com/wrmsr/presto/launcher/util/DaemonProcess.java
@@ -83,6 +83,7 @@ public class DaemonProcess
 
     public synchronized int clearPid()
     {
+        refresh();
         checkState(locked, "pid file not locked by us");
         // FIXME err check
         posix.lseek(pidFile, 0, 0);
