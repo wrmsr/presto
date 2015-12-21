c36f5e80e6e01ddc8031021ee4d1a6df91460417
wrmsr <timwilloney@gmail.com>
2015-12-20 19:12:37 -0800

    --

--- presto-wrmsr-utils/src/main/java/com/wrmsr/presto/util/config/mergeable/MergeableConfig.java
+++ presto-wrmsr-utils/src/main/java/com/wrmsr/presto/util/config/mergeable/MergeableConfig.java
@@ -14,10 +14,14 @@
 package com.wrmsr.presto.util.config.mergeable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Iterables;
 import com.wrmsr.presto.util.Mergeable;
 import com.wrmsr.presto.util.Serialization;
 
 import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.Maps.newHashMap;
 
 public interface MergeableConfig<N extends MergeableConfig>
         extends Mergeable
@@ -27,9 +31,13 @@ public interface MergeableConfig<N extends MergeableConfig>
     default Mergeable merge(Mergeable other)
     {
         ObjectMapper mapper = Serialization.OBJECT_MAPPER.get();
-        Map newMap = Serialization.roundTrip(mapper, this, Map.class);
-        Map<Object, Object> otherMap = Serialization.roundTrip(mapper, other, Map.class);
-        for (Map.Entry<Object, Object> entry : otherMap.entrySet()) {
+        Map<String, Map> m = Serialization.roundTrip(mapper, this, Map.class);
+        String name = Iterables.getOnlyElement(m.keySet());
+        Map<String, Object> newMap = newHashMap(Iterables.getOnlyElement(m.values()));
+        m = Serialization.roundTrip(mapper, other, Map.class);
+        checkArgument(name.equals(Iterables.getOnlyElement(m.keySet())));
+        Map<String, Object> otherMap = Iterables.getOnlyElement(m.values());
+        for (Map.Entry<String, Object> entry : otherMap.entrySet()) {
             newMap.put(entry.getKey(), entry.getValue());
         }
         return Serialization.roundTrip(mapper, otherMap, (Class<N>) getClass());
