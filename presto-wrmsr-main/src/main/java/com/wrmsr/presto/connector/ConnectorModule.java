051fdc370ca8d41ba40d071be6e11358d9e9535d
wrmsr <timwilloney@gmail.com>
2016-01-14 23:22:29 -0800

    --

--- presto-wrmsr-main/src/main/java/com/wrmsr/presto/connector/ConnectorModule.java
+++ presto-wrmsr-main/src/main/java/com/wrmsr/presto/connector/ConnectorModule.java
@@ -16,55 +16,49 @@ package com.wrmsr.presto.connector;
 import com.facebook.presto.spi.connector.ConnectorFactory;
 import com.google.inject.Binder;
 import com.google.inject.Module;
-import com.google.inject.multibindings.Multibinder;
 import com.wrmsr.presto.connector.jdbc.h2.H2ConnectorFactory;
 import com.wrmsr.presto.connector.jdbc.mysql.ExtendedMySqlConnectorFactory;
 import com.wrmsr.presto.connector.jdbc.postgresql.ExtendedPostgreSqlConnectorFactory;
 import com.wrmsr.presto.connector.jdbc.redshift.RedshiftConnectorFactory;
 import com.wrmsr.presto.connector.jdbc.sqlite.SqliteConnectorFactory;
 import com.wrmsr.presto.connector.jdbc.temp.TempConnectorFactory;
-import com.wrmsr.presto.connector.partitioner.PartitionerConnectorFactory;
 import com.wrmsr.presto.connector.partitioner.PartitionerMetaconnectorFactory;
 import com.wrmsr.presto.connector.views.ViewAnalyzer;
 import com.wrmsr.presto.connector.views.ViewsConnectorFactory;
 
+import static com.google.inject.multibindings.Multibinder.newSetBinder;
+
 public class ConnectorModule
-    implements Module
+        implements Module
 {
     @Override
     public void configure(Binder binder)
     {
-        Multibinder<ConnectorFactory> connectorFactoryBinder = Multibinder.newSetBinder(binder, ConnectorFactory.class);
-
         binder.bind(ViewAnalyzer.class).asEagerSingleton();
         binder.bind(ViewsConnectorFactory.class).asEagerSingleton();
-        connectorFactoryBinder.addBinding().to(ViewsConnectorFactory.class);
-
-        Multibinder<com.facebook.presto.spi.ConnectorFactory> legacyConnectorFactoryBinder = Multibinder.newSetBinder(binder, com.facebook.presto.spi.ConnectorFactory.class);
+        newSetBinder(binder, ConnectorFactory.class).addBinding().to(ViewsConnectorFactory.class);
 
         binder.bind(H2ConnectorFactory.class).asEagerSingleton();
-        legacyConnectorFactoryBinder.addBinding().to(H2ConnectorFactory.class);
+        newSetBinder(binder, com.facebook.presto.spi.ConnectorFactory.class).addBinding().to(H2ConnectorFactory.class);
 
         binder.bind(ExtendedMySqlConnectorFactory.class).asEagerSingleton();
-        legacyConnectorFactoryBinder.addBinding().to(ExtendedMySqlConnectorFactory.class);
+        newSetBinder(binder, com.facebook.presto.spi.ConnectorFactory.class).addBinding().to(ExtendedMySqlConnectorFactory.class);
 
         binder.bind(ExtendedPostgreSqlConnectorFactory.class).asEagerSingleton();
-        legacyConnectorFactoryBinder.addBinding().to(ExtendedPostgreSqlConnectorFactory.class);
+        newSetBinder(binder, com.facebook.presto.spi.ConnectorFactory.class).addBinding().to(ExtendedPostgreSqlConnectorFactory.class);
 
         binder.bind(RedshiftConnectorFactory.class).asEagerSingleton();
-        legacyConnectorFactoryBinder.addBinding().to(RedshiftConnectorFactory.class);
+        newSetBinder(binder, com.facebook.presto.spi.ConnectorFactory.class).addBinding().to(RedshiftConnectorFactory.class);
 
         binder.bind(SqliteConnectorFactory.class).asEagerSingleton();
-        legacyConnectorFactoryBinder.addBinding().to(SqliteConnectorFactory.class);
+        newSetBinder(binder, com.facebook.presto.spi.ConnectorFactory.class).addBinding().to(SqliteConnectorFactory.class);
 
         binder.bind(TempConnectorFactory.class).asEagerSingleton();
-        legacyConnectorFactoryBinder.addBinding().to(TempConnectorFactory.class);
+        newSetBinder(binder, com.facebook.presto.spi.ConnectorFactory.class).addBinding().to(TempConnectorFactory.class);
 
         binder.bind(MetaconnectorManager.class).asEagerSingleton();
 
-        Multibinder<MetaconnectorFactory> metaconnectorFactoryBinder = Multibinder.newSetBinder(binder, MetaconnectorFactory.class);
-
         binder.bind(PartitionerMetaconnectorFactory.class).asEagerSingleton();
-        metaconnectorFactoryBinder.addBinding().to(PartitionerMetaconnectorFactory.class);
+        newSetBinder(binder, MetaconnectorFactory.class).addBinding().to(PartitionerMetaconnectorFactory.class);
     }
 }
