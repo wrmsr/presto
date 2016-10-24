7100c8febbcd7549a1d01498c513b8107bfa9f86
wrmsr <timwilloney@gmail.com>
2016-10-24 09:50:44 -0700

    --

--- presto-fusion-launcher/src/main/java/com/wrmsr/presto/launcher/packaging/Packager2.java
+++ presto-fusion-launcher/src/main/java/com/wrmsr/presto/launcher/packaging/Packager2.java
@@ -15,10 +15,15 @@ package com.wrmsr.presto.launcher.packaging;
 
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
-import com.sun.org.apache.xerces.internal.dom.DeferredElementImpl;
+import com.google.common.collect.ImmutableMap;
 import io.airlift.resolver.ArtifactResolver;
+import io.airlift.resolver.DefaultArtifact;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.DefaultModelReader;
+import org.apache.maven.model.io.ModelReader;
+import org.sonatype.aether.artifact.Artifact;
+import org.sonatype.aether.artifact.ArtifactType;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 import javax.annotation.concurrent.Immutable;
@@ -28,30 +33,103 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
+import static com.wrmsr.presto.util.collect.MoreCollectors.toArrayList;
+import static com.wrmsr.presto.util.collect.MoreCollectors.toImmutableList;
+import static java.util.Comparator.comparing;
 import static java.util.Objects.requireNonNull;
 
 public final class Packager2
 {
     private final ArtifactResolver artifactResolver;
+    private final List<ArtifactTransform> artifactTransforms;
+
+    @FunctionalInterface
+    public interface ArtifactTransform
+    {
+        List<Artifact> apply(Packager2 packager, List<Artifact> artifacts);
+    }
+
+    @Immutable
+    public static final class IgnoreArtifactTransform
+            implements ArtifactTransform
+    {
+        private final List<Predicate<Artifact>> ignorePredicates;
+
+        public IgnoreArtifactTransform(List<Predicate<Artifact>> ignorePredicates)
+        {
+            this.ignorePredicates = ImmutableList.copyOf(ignorePredicates);
+        }
+
+        @Override
+        public List<Artifact> apply(Packager2 packager, List<Artifact> artifacts)
+        {
+            return artifacts.stream()
+                    .filter(artifact -> !ignorePredicates.stream().anyMatch(predicate -> predicate.test(artifact)))
+                    .collect(toArrayList());
+        }
+    }
+
+    @Immutable
+    public static final class MatchVersionsArtifactTransform
+            implements ArtifactTransform
+    {
+        public static final Function<List<Artifact>, String> MAX_BY_STRING_VERSION =
+                artifacts -> artifacts.stream().collect(Collectors.maxBy(comparing(Artifact::getVersion))).get().getVersion();
+
+        private final Predicate<Artifact> predicate;
+        private final Function<List<Artifact>, String> versionFunciton;
+
+        public MatchVersionsArtifactTransform(Predicate<Artifact> predicate, Function<List<Artifact>, String> versionFunciton)
+        {
+            this.predicate = requireNonNull(predicate);
+            this.versionFunciton = requireNonNull(versionFunciton);
+        }
+
+        @Override
+        public List<Artifact> apply(Packager2 packager, List<Artifact> artifacts)
+        {
+            Map<Boolean, List<Artifact>> groups = artifacts.stream().collect(Collectors.partitioningBy(predicate));
+            artifacts = new ArrayList<>(groups.getOrDefault(false, ImmutableList.of()));
+
+            if (groups.containsKey(true)) {
+                List<Artifact> matchingArtifacts = groups.get(true);
+                if (!matchingArtifacts.isEmpty()) {
+                    String version = versionFunciton.apply(matchingArtifacts);
+                    List<Artifact> newArtifacts = matchingArtifacts.stream()
+                            .map(a -> new DefaultArtifact(a.getGroupId(), a.getArtifactId(), a.getClassifier(), a.getExtension(), version, a.getProperties(), (ArtifactType) null))
+                            .collect(toImmutableList());
+                    List<Artifact> resolvedNewArtifacts = packager.artifactResolver.resolveArtifacts(newArtifacts);
+                    artifacts.addAll(resolvedNewArtifacts);
+                }
+            }
+
+            return artifacts;
+        }
+    }
 
     @Immutable
     private static final class Module
     {
         private final File pomFile;
-        private final Document pom;
+        private final Model model;
         private final String name;
         private final Set<String> classPath;
 
-        public Module(File pomFile, Document pom, String name, Set<String> classPath)
+        public Module(File pomFile, Model model, String name, Set<String> classPath)
         {
             this.pomFile = pomFile;
-            this.pom = pom;
+            this.model = model;
             this.name = name;
             this.classPath = classPath;
         }
@@ -60,9 +138,12 @@ public final class Packager2
     private final Map<String, Module> modules = new HashMap<>();
     private Module mainModule;
 
-    public Packager2(ArtifactResolver artifactResolver)
+    public Packager2(
+            ArtifactResolver artifactResolver,
+            List<ArtifactTransform> artifactTransforms)
     {
         this.artifactResolver = requireNonNull(artifactResolver);
+        this.artifactTransforms = ImmutableList.copyOf(artifactTransforms);
     }
 
     public void addMainModule(File pomFile)
@@ -104,14 +185,13 @@ public final class Packager2
     {
         checkArgument(pomFile.isFile());
 
-        Document pom = readXml(pomFile);
+        Model model = new DefaultModelReader().read(pomFile, ImmutableMap.of(ModelReader.IS_STRICT, true));
 
-        // checkState(!modules.containsKey(pom));
+        List<Artifact> artifacts = artifactResolver.resolvePom(pomFile).stream().collect(toArrayList());
+        for (ArtifactTransform artifactTransform : artifactTransforms) {
+            artifacts = artifactTransform.apply(this, artifacts);
+        }
 
-        System.out.println(pom);
-
-        Node groupId = pom.getDocumentElement().getChildNodes().item(5);
-        pom.getDocumentElement().getElementsByTagNameNS(groupId.getNamespaceURI(), groupId.getNodeName());
         throw new IllegalArgumentException();
     }
 
@@ -122,7 +202,12 @@ public final class Packager2
                 ArtifactResolver.USER_LOCAL_REPO,
                 ImmutableList.of(ArtifactResolver.MAVEN_CENTRAL_URI));
 
-        Packager2 p = new Packager2(resolver);
+        List<ArtifactTransform> artifactTransforms = ImmutableList.of(
+                new MatchVersionsArtifactTransform(
+                        a -> "org.slf4j".equals(a.getGroupId()),
+                        MatchVersionsArtifactTransform.MAX_BY_STRING_VERSION));
+
+        Packager2 p = new Packager2(resolver, artifactTransforms);
         p.addMainModule(new File("/Users/spinlock/src/wrmsr/presto/presto-fusion-launcher/pom.xml"));
     }
 }
